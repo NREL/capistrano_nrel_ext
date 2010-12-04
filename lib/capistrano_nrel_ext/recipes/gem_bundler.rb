138195462b2df03979175cc2df57f4b0d1087587
Muerdter, Nick <nick.muerdter@nrel.gov>
2010-12-04 00:47:57 +0000

    A new gem bundler recipe for Bundler 1.0. Based on the cap recipe bundler provides, but customized to handle our multiple apps.

--- lib/capistrano_nrel_ext/recipes/gem_bundler.rb
+++ lib/capistrano_nrel_ext/recipes/gem_bundler.rb
@@ -14,22 +14,55 @@ Capistrano::Configuration.instance(true).load do
   #
   namespace :deploy do
     namespace :gem_bundler do
+      send :desc, <<-DESC
+        Install the current Bundler environment. This is based on Bundler's \
+        own capistrano task, but customized to handle our multiple \
+        applications in a single deployment.
+
+        By default, gems will be installed to the shared/bundle path. Gems in \
+        the development and test group will not be installed. The install \
+        command is executed with the --deployment and --quiet flags. If the \
+        bundle cmd cannot be found then you can override the bundle_cmd \
+        variable to specifiy which one it should use.
+
+        You can override any of these defaults by setting the variables shown below.
+
+          set :bundle_gemfile,  "Gemfile"
+          set :bundle_dir,      File.join(fetch(:shared_path), 'bundle')
+          set :bundle_flags,    "--deployment --quiet"
+          set :bundle_without,  [:development, :test]
+          set :bundle_cmd,      "bundle" # e.g. "/opt/ruby/bin/bundle"
+      DESC
       task :install, :except => { :no_release => true } do
+        # Gather all the paths specifically defined as Bundler apps.
         gem_bundler_paths = gem_bundler_apps.collect do |application_path|
           File.join(latest_release, application_path)
         end
 
+        # Also add all the paths to Rails apps that might use Bundler.
         if(exists?(:all_rails_applications))
           gem_bundler_paths += all_rails_applications.collect do |application_path, public_path|
             File.join(latest_release, application_path)
           end
         end
 
-        gem_bundler_paths.each do |application_path|
-          if(remote_file_exists?(File.join(application_path, "Gemfile")))
-            run "cd #{application_path} && " +
-              "mkdir -p #{application_path}/vendor && " +
-              "bundle install vendor/bundle"
+        bundle_cmd     = fetch(:bundle_cmd, "bundle")
+        bundle_flags   = fetch(:bundle_flags, "--deployment --quiet")
+        bundle_gemfile = fetch(:bundle_gemfile, "Gemfile")
+        bundle_without = [*fetch(:bundle_without, [:development, :test])].compact
+
+        gem_bundler_paths.each do |full_application_path|
+          gemfile_path = File.join(full_application_path, bundle_gemfile)
+
+          if(remote_file_exists?(gemfile_path))
+            bundle_dir     = fetch(:bundle_dir, File.join(full_application_path, "vendor", "bundle"))
+
+            args = ["--gemfile #{gemfile_path}"]
+            args << "--path #{bundle_dir}" unless bundle_dir.to_s.empty?
+            args << bundle_flags.to_s
+            args << "--without #{bundle_without.join(" ")}" unless bundle_without.empty?
+
+            run "#{bundle_cmd} install #{args.join(' ')}"
           end
         end
       end
