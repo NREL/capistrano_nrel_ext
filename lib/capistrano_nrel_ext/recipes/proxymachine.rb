3c7c412ddb107a6ee36f95cb797835e188d26aa0
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-01-13 05:18:27 +0000

    Get rid of unused proxymachine recipe.

--- lib/capistrano_nrel_ext/recipes/proxymachine.rb
+++
@@ -1,9 +0,0 @@
-Capistrano::Configuration.instance(true).load do
-  #
-  # Variables
-  #
-  set :proxymachine_servers, []
-  set(:proxymachine_conf_file) { File.join(latest_release, "config", "proxymachine.yml") }
-  set(:proxymachine_pid_dir) { File.join(current_release, "tmp", "pids") }
-  set(:proxymachine_log_dir) { File.join(current_release, "log") }
-end
