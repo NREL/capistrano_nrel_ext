747599145cdb36485010901363f25461217597c0
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-01-13 05:37:53 +0000

    Recipe for installing nginx configuration files.

---
+++ lib/capistrano_nrel_ext/recipes/nginx.rb
@@ -0,0 +1,75 @@
+require "capistrano_nrel_ext/actions/remote_tests"
+require "capistrano_nrel_ext/actions/sample_files"
+
+Capistrano::Configuration.instance(true).load do
+  #
+  # Variables
+  #
+  _cset :nginx_conf_dir, "/etc/nginx/sites-available"
+  _cset :nginx_init_script, "/etc/init.d/nginx"
+  _cset :nginx_user, "www-data"
+  _cset :nginx_applications, ["."]
+
+  #
+  # Hooks 
+  #
+  after "deploy:update_code", "deploy:nginx:config"
+  before "deploy:restart", "deploy:nginx:install"
+  after "deploy:restart", "nginx:reload"
+  before "undeploy:delete", "undeploy:nginx:delete"
+  after "undeploy", "nginx:reload"
+
+  #
+  # Tasks
+  #
+  namespace :nginx do
+    desc <<-DESC
+      Restart Nginx. This should be executed if new Nginx configuration
+      files have been deployed.
+    DESC
+    task :reload, :roles => :app, :except => { :no_release => true } do
+      sudo "#{nginx_init_script} reload"
+    end
+  end
+
+  namespace :deploy do
+    namespace :nginx do
+      desc <<-DESC
+        Create the Nginx configuration file. If a sample file for the given
+        stage is present in config/nginx, the sample is run through ERB (for
+        variable replacement) to create the actual config file to be used.
+      DESC
+      task :config, :except => { :no_release => true } do
+        parse_sample_files(["config/nginx"])
+      end
+
+      desc <<-DESC
+        Install the Nginx configuration file in a system-wide directory for
+        Nginx to find. This makes a symbolic link to the latest configuration
+        file for this deployment in Nginx's configuration directory.
+      DESC
+      task :install, :except => { :no_release => true } do
+        conf_file = "#{latest_release}/config/nginx/#{stage}.conf"
+        if(remote_file_exists?(conf_file))
+          run "ln -sf #{conf_file} #{nginx_conf_dir}/#{deploy_name}"
+          run "nxensite #{deploy_name}"
+        end
+      end
+    end
+  end
+
+  namespace :undeploy do
+    namespace :nginx do
+      # Remove the symbolic link to the nginx configuration file that's in
+      # place for this deployment.
+      task :delete do
+        begin
+          run "nxdissite #{deploy_name}"
+        rescue Capistrano::CommandError
+        end
+
+        run "rm -f #{nginx_conf_dir}/#{deploy_name}"
+      end
+    end
+  end
+end
