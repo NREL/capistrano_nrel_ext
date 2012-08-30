00de4091d1ec768b47aeb55ca319f7e445a318ec
Nick Muerdter <nick.muerdter@nrel.gov>
2012-08-30 11:51:56 -0600

    Try clearing the APC cache twice.

--- lib/capistrano_nrel_ext/recipes/php_apc.rb
+++ lib/capistrano_nrel_ext/recipes/php_apc.rb
@@ -15,11 +15,26 @@ Capistrano::Configuration.instance(true).load do
     DESC
     task :clear_cache, :roles => :app, :except => { :no_release => true } do
       apc_clear_cache_path = File.join(current_path, "public/apc_clear_cache.php")
-      run <<-CMD
-        echo "<?php header('Cache-Control: max-age=0'); apc_clear_cache(); apc_clear_cache('user'); ?>" > #{apc_clear_cache_path} && \
-        curl -s "http://#{domain}/apc_clear_cache.php?cache_buster=#{Time.now.to_f}"; \
-        rm -f #{apc_clear_cache_path};
-      CMD
+
+      # Run the cache clearing command twice. This shouldn't be necessary, but
+      # it seems to be for now.
+      #
+      # I think this relates to us not actually reloading the php-fpm processes
+      # on deployment (because the reload process isn't very graceful:
+      # https://bugs.php.net/bug.php?id=60961). Since we don't perform a
+      # reload, I think the first PHP hit sort of internally reloads things,
+      # and then the second one will actually clear the APC cache properly. At
+      # least that's my theory.
+      #
+      # This should probably be revisited if php-fpm's reload process becomes
+      # more graceful.
+      2.times do
+        run <<-CMD
+          echo "<?php header('Cache-Control: max-age=0'); apc_clear_cache(); apc_clear_cache('user'); ?>" > #{apc_clear_cache_path} && \
+          curl -s "http://#{domain}/apc_clear_cache.php?cache_buster=#{Time.now.to_f}"; \
+          rm -f #{apc_clear_cache_path};
+        CMD
+      end
     end
   end
 end
