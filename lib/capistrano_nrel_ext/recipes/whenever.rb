3085cc267c61d529c70dc31e79f51f9a42979255
Nick Muerdter <nick.muerdter@nrel.gov>
2013-05-09 19:15:29 -0600

    Add recipe for whenever gem.

---
+++ lib/capistrano_nrel_ext/recipes/whenever.rb
@@ -0,0 +1,16 @@
+require "capistrano_nrel_ext/recipes/gem_bundler"
+require "whenever/capistrano/recipes"
+
+# This just shifts whenever's default capistrano recipes to happen slightly
+# later so it gets along with when we install gems (after deploy:update_code
+# instead of before deploy:finalize_update).
+#
+# (This could be revisited if we shift when we install gems, but at this point,
+# that might be a bit tricky.)
+Capistrano::Configuration.instance(true).load do
+  # Write the new cron jobs near the end.
+  after "deploy:update_code", "whenever:update_crontab"
+
+  # If anything goes wrong, undo.
+  after "deploy:rollback", "whenever:update_crontab"
+end
