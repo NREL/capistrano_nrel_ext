ccbaa287fa401a5a48d119c7c7b23c6f6680d751
Nick Muerdter <nick.muerdter@nrel.gov>
2014-04-14 22:51:07 -0600

    Sort out the lock file sticking around erroneously.

--- lib/capistrano_nrel_ext/recipes/locked.rb
+++ lib/capistrano_nrel_ext/recipes/locked.rb
@@ -1,4 +1,34 @@
 require "time"
+require "capistrano/cli"
+
+class Capistrano::CLI
+  # Monkey patch Capistrano's internal execute_requested_actions method to
+  # ensure the lock file gets cleaned up on deployment failures.
+  #
+  # We normally rely on the `after "deploy"` callback to cleanup the lock file,
+  # but if the deployment fails at a variety of other places during setup or in
+  # before tasks, the after callbacks never get called. To work around that
+  # issue, we intercept all exceptions raised by the Capistrano tasks and
+  # cleanup the lock file as appropriate.
+  def execute_requested_actions_with_lock_cleanup(config)
+    begin
+      execute_requested_actions_without_lock_cleanup(config)
+    rescue => e
+      # Only cleanup the lock file if the lock file was created during this
+      # capistrano run (since we don't want to remove it if the error being
+      # raised is actually the error about the lock file existing).
+      if(config.fetch(:cleanup_lock_on_error, false))
+        config.find_and_execute_task("deploy:unlock")
+      end
+
+      raise e
+    end
+  end
+
+  alias_method :execute_requested_actions_without_lock_cleanup, :execute_requested_actions
+  alias_method :execute_requested_actions, :execute_requested_actions_with_lock_cleanup
+end
+
 
 Capistrano::Configuration.instance(true).load do
   #
@@ -42,6 +72,7 @@ finishes.
     cap ENVIRONMENT deploy:unlock
           eos
         else
+          set :cleanup_lock_on_error, true
           put(user, lock_file)
         end
       end
