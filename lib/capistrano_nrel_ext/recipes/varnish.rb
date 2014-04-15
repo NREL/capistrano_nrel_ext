bd37f16a21f678f761372184914255709c79c640
Nick Muerdter <nick.muerdter@nrel.gov>
2014-04-14 20:44:29 -0600

    Standardize how sudo gets called on the varnish scripts.

--- lib/capistrano_nrel_ext/recipes/varnish.rb
+++ lib/capistrano_nrel_ext/recipes/varnish.rb
@@ -18,7 +18,7 @@ Capistrano::Configuration.instance(true).load do
       deployments.
     DESC
     task :ban, :roles => :app, :except => { :no_release => true } do
-      commands = all_domains.collect { |d| %(sudo #{varnish_ban_script} 'req.http.host == "#{d}"') }
+      commands = all_domains.collect { |d| %(#{sudo} #{varnish_ban_script} 'req.http.host == "#{d}"') }
       run commands.join(" && ")
     end
   end
